1683ba7be7ba3d482c836759cd4ecddfcb7f2684
Akel <akel.obied@itellu.ai>
2025-09-27 22:36:42 +0300

    update mongo DB connection

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TodoModule } from './todo/todo.module';
+import { TodoModule } from './todo/todo.module'; // Step 1: Import TodoModule
 
 @Module({
-  imports: [TodoModule],
+  imports: [
+    MongooseModule.forRoot('mongodb+srv://akel2_db_user:Mindakob5@cluster0.co4gmnz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'),
+    TodoModule, // עדכן כאן את ה-URI
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
